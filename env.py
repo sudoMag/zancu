f68134c63b0e0817d34ec79e8fa2c8977373ae1e
mag <m.casa2017@gmail.com>
2021-07-04 12:07:59 -0400

    genius svg image added to landing page

--- env.py
+++ env.py
@@ -7,4 +7,5 @@ SCRIPTS_RUTE = "%s/static/js/" % _dirname
 STYLES_RUTE = "%s/static/css/" % _dirname
 MEDIA_RUTE = "%s/static/media/" % _dirname
 SRC_FILES_RUTE = "%s/src/" % _dirname
+SRC_MEDIA_RUTE = "%s/src/landingpage/media/" % _dirname
 HOST_IP = "192.168.137.118"
--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request, jsonify, send_file, send_from_directory, redirect
-from env import STYLES_RUTE, SCRIPTS_RUTE, HOST_IP, SRC_FILES_RUTE
+from env import STYLES_RUTE, SCRIPTS_RUTE, HOST_IP, SRC_FILES_RUTE, SRC_MEDIA_RUTE
 
 app = Flask(__name__)
 
@@ -27,9 +27,11 @@ def srcFiles (folder, route, filename):
 	return send_from_directory(SRC_FILES_RUTE + folder + "/" + route, filename)
 
 
+@app.route("/src/media/<filename>")
+def mediaFiles(filename):
+	return send_from_directory(SRC_MEDIA_RUTE, filename)
+
+
 if __name__ == "__main__":
 	app.run(debug=True)
 	#app.run(port=3000, host=HOST_IP, debug=True)
-
-
-
